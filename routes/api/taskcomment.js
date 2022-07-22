cdd4c01eb269102551b5b5b0ea245eb5d94e5b88
Senbiki-Akifumi <a-senbiki@sunmark.bz>
2022-07-22 23:34:32 +0900

    指定したタスクにコメントを登録するAPIを一旦完了 後ほど清書

--- routes/api/taskcomment.js
+++ routes/api/taskcomment.js
@@ -3,29 +3,78 @@ import express from 'express'
 let router = express.Router()
 import validationRules from '../../config/validationRules.js'
 import { check, validationResult } from 'express-validator'
-import models from "../../models/index.js";
+import models from '../../models/index.js'
+
 /**
  * 指定したプロジェクトIDにタスクコメントを追加するAPI
+ *
  */
 router.post('/create/:projectId', validationRules['taskComment.create'], (req, res, next) => {
-
   const errors = validationResult(req)
-  console.log(errors.array())
-  let postData = req.body;
-  let projectId = req.params.projectId
-  console.log(postData);
+  // console.log(errors.array())
+  let postData = req.body
+  let imageIdList = req.body.image_id_list
+  // console.log(postData)
   const init = async function () {
-    let taskComment = await models.TaskComment.create( {
+
+    /**
+     *
+     * @type {Transaction}
+     */
+    let tx = await models.sequelize.transaction(null, null)
+    let taskComment = await models.TaskComment.create({
       task_id: postData.task_id,
       user_id: postData.user_id,
       comment: postData.comment,
+    }, { transaction: tx })
+    console.log(taskComment)
+    // Fetch the last insert id.
+    let taskCommentId = taskComment.id
+    let uniqueImageIdList = []
+    console.log(imageIdList);
+    imageIdList.filter((value, index) => {
+      if ( uniqueImageIdList.indexOf(value) === -1 ) {
+        uniqueImageIdList.push(value)
+      }
+    })
+    console.log(uniqueImageIdList);
+    let commentImagesForBulk = []
+    uniqueImageIdList.forEach((value) => {
+      commentImagesForBulk.push({
+        comment_id: taskCommentId,
+        image_id: value,
+      })
+    })
+    console.log(commentImagesForBulk)
+    let commentImages = await models.CommentImage.bulkCreate(commentImagesForBulk, {transaction: tx});
+    console.log(commentImages);
+    if (commentImages.length !== commentImagesForBulk.length) {
+      throw new Error("Failed inserting comment images.");
+    }
+    // commit.
+    await tx.commit();
+    return models.TaskComment.findByPk(taskCommentId, {
+      include: [
+        {
+          model: models.CommentImage,
+        }
+      ]
     });
-    console.log(taskComment);
   }
+  // Return the API response.
   init().then((result) => {
-
+    return res.send({
+      status: true,
+      code: 200,
+      response: result,
+    });
   }).catch((error) => {
-    console.log(error);
+    console.log(error)
+    return res.send({
+      status: false,
+      code: 400,
+      response: error,
+    });
   })
 })
 
